221fc6c07be59e5b9f9940e2cdc0617f3bfd3158
Jorge Lizarraga <lizarraga-jorge@live.com>
2018-06-14 14:41:09 -0500

    Add Requests PAge
    Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CounterComponent } from './counter.component';
 import { HomeComponent } from './home.component';
+import { RequestsComponent } from './requests.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CounterComponent,
-    HomeComponent
+    HomeComponent, 
+    RequestsComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/requests.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { Request } from './Request';
+
+@Component({
+  selector: 'requests-page',
+  templateUrl: './requests.component.html',
+  styleUrls: ['./app.component.css']
+})
+
+export class RequestsComponent
+{
+  title = 'app';
+  
+  heroes = [
+    new Request("Brandon", "Buchanan", "Golf"),
+    new Request("Jorge", "Garcia", "Rocket League"),
+    new Request("Drake", "Loud", "Coffee"),
+    new Request("Pierre", "Pierre", "Crossaint"),
+    new Request("Brandon", "Buchanan", "Golf"),
+    new Request("Jorge", "Garcia", "Rocket League"),
+    new Request("Drake", "Loud", "Coffee"),
+    new Request("Pierre", "Pierre", "Crossaint")
+  ]
+}
